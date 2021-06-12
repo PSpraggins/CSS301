fb573f5e32d4507401949ff6e8531c880a0d3e7f
Patricia Spraggins <pdellongo@gmail.com>
2021-06-11 22:34:03 -0500

    Update jQuery Functions
    Made the page to not scroll when overlay was on top.

--- jquery_functions.js
+++ jquery_functions.js
@@ -1,10 +1,14 @@
 $(document).ready(function(){
     $("div.collapsed-menu").click(function() {
         $(".side-nav").css("display", "block");
+        $("body").css("overflow", "hidden");
+        $("body").css("scroll", "no");
     });
 });
 $(document).ready(function(){
     $("div.menu-close").click(function() {
         $(".side-nav").hide();
+        $("body").css("overflow", "scroll");
+        $("body").css("scroll", "yes");
     });
 });
