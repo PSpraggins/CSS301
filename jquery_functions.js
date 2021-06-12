eae7376bdec22db613c8ce100772786f135b680b
Patricia Spraggins <pdellongo@gmail.com>
2021-06-11 22:22:04 -0500

    Adding Media Queries

--- jquery_functions.js
+++ jquery_functions.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
     $("div.collapsed-menu").click(function() {
-        $("#side-nav").css("display", "block");
+        $(".side-nav").css("display", "block");
     });
 });
 $(document).ready(function(){
     $("div.menu-close").click(function() {
-        $("#side-nav").hide();
+        $(".side-nav").hide();
     });
 });
